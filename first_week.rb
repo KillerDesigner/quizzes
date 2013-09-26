8e049fab8d90c87275c827cbda6943aaaa061115
Feather <lisafeather@gmail.com>
2013-09-26 14:14:02 -0700

    please commit

---
+++ first_week.rb
@@ -0,0 +1,70 @@
+                                                             
+#     ,o888888o.     8 8888      88  8 8888  8888888888',8888' 
+#  . 8888     `88.   8 8888      88  8 8888         ,8',8888'  
+# ,8 8888       `8b  8 8888      88  8 8888        ,8',8888'   
+# 88 8888        `8b 8 8888      88  8 8888       ,8',8888'    
+# 88 8888         88 8 8888      88  8 8888      ,8',8888'     
+# 88 8888     `8. 88 8 8888      88  8 8888     ,8',8888'      
+# 88 8888      `8,8P 8 8888      88  8 8888    ,8',8888'       
+# `8 8888       ;8P  ` 8888     ,8P  8 8888   ,8',8888'        
+#  ` 8888     ,88'8.   8888   ,d8P   8 8888  ,8',8888'         
+#     `8888888P'  `8.   `Y88888P'    8 8888 ,8',8888888888888  
+
+#     
+# This is our first week's quiz! Let's find out what we know.
+# 
+
+# The ideal range of your motor cycle speed 20 - 55. Over 55 is SCAREE!
+# Check if your moto_speed is within that range using boolean (&&, ||)
+# operators and comparison operators (== =< >= !=)
+
+moto_speed = ?
+
+# if your moto_speed variable is in the right range print out a good
+# message, aka "Wheee!" Otherwise print out an appropriate response.
+# Your code goes below:
+
+
+
+# Make a method to start your bike! It should print out "vrooom!"
+# when it's called
+
+def make_vroom
+  
+end
+
+# You're the leader of the pack. 
+# Create an Array of motorcycle makes!
+
+my_convoy = []
+
+# Loop through your convoy and print out each motorcycle's make
+# Your code below:
+
+
+
+# You need to keep track of your gang. 
+# Create a Hash containing riders' info.
+
+my_gang = {}
+
+# Loop through your gang and print out each rider's name
+# Your code below:
+
+
+# Define an Class to represent each gang member 
+# It should include methods to set their name and motorcyle make
+# Give it some useful methods! For example, when say_name(rider) 
+# is called the rider's name is printed out
+
+Class Rider
+  def initialize(name, moto_model)
+  end
+
+  def say_name(rider)
+  end
+end
+
+# Create a Hash of Rider objects! Then loop through the Hash
+# and call the say_name of each rider.
+# Your code below:
